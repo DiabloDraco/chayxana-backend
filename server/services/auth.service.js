d4cd346cbaf7cb84350cd73a3dd08ec8f2943b32
DiabloDraco <mirmuhsinmirahmatov@gmail.com>
2024-10-29 11:36:18 +0500

    add

--- server/services/auth.service.js
+++ server/services/auth.service.js
@@ -111,6 +111,13 @@ const findAllRoles = async () => {
 
 const registerUser = async ({ name, password, phone, referal, mail }) => {
   try {
+    const isMail = await UserModel.findOne({
+      where: { mail },
+    });
+
+    if (isMail) {
+      throw new Error("Mail already exists");
+    }
     if (referal) {
       const parent = await UserModel.findOne({
         where: { referralCode: referal },
